60b37c6cb28082e07e24d130f0aefc9491cc576a
alexandre95calonne <alexandre.calonne@epitech.eu>
2024-12-26 22:00:39 +0100

    chore: changed email

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:alexandre95calonne@gmail.com">
+        <a href="mailto:alexandre.calonne95@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
